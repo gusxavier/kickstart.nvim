d73058559c23e69273c53aaee5c337ef6d16b585
Gustavo Xavier <gustavo.xavier@180s.com.br>
2024-04-20 14:22:13 -0300

    Initial config + custom plugins

--- init.lua
+++ init.lua
@@ -88,10 +88,10 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = ','
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -567,6 +567,10 @@ require('lazy').setup({
         -- tsserver = {},
         --
 
+        clojure_lsp = {
+          single_file_support = true,
+        },
+
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -818,7 +822,19 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'clojure',
+        'csv',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'typescript',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -829,6 +845,15 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<CR>',
+          scope_incremental = '<CR>',
+          node_incremental = '<TAB>',
+          node_decremental = '<S-TAB>',
+        },
+      },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -859,16 +884,16 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,97 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'NeogitOrg/neogit',
+    branch = 'nightly',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = true,
+    cmd = 'Neogit',
+    keys = {
+      { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit' },
+      { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'Diffview open' },
+    },
+  },
+  {
+    'ruifm/gitlinker.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = function()
+      require('gitlinker').setup { mappings = nil }
+    end,
+    keys = {
+      {
+        '<leader>gl',
+        "<cmd>lua require('gitlinker').get_buf_range_url('n')<cr>",
+        mode = 'n',
+        desc = 'Git link line',
+      },
+      {
+        '<leader>gl',
+        "<cmd>lua require('gitlinker').get_buf_range_url('v')<cr>",
+        mode = 'v',
+        desc = 'Git link range',
+      },
+      {
+        '<leader>gL',
+        "<cmd>lua require('gitlinker').get_repo_url({action_callback = require('gitlinker.actions').open_in_browser})<cr>",
+        mode = 'v',
+        desc = 'Git open repo homepage',
+      },
+    },
+  },
+  {
+    'echasnovski/mini.files',
+    config = true,
+    opts = {
+      mappings = {
+        go_in = 'L',
+        go_in_plus = 'l',
+      },
+    },
+    keys = {
+      { '<leader>e', '<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>', desc = 'Mini.files open current file dir' },
+      { '<leader>E', '<cmd>lua MiniFiles.open()<cr>', desc = 'Mini.files open' },
+    },
+  },
+  {
+    'guns/vim-sexp',
+    ft = 'clojure',
+  },
+  {
+    'tpope/vim-sexp-mappings-for-regular-people',
+    ft = 'clojure',
+  },
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    ft = 'clojure',
+    config = function()
+      require('rainbow-delimiters.setup').setup {}
+    end,
+  },
+  {
+    'Olical/conjure',
+    ft = { 'clojure' },
+    config = function()
+      vim.g['conjure#client#clojure#nrepl#test#current_form_names'] = {
+        'deftest',
+        'defspec',
+        'defflow',
+        'init-flow',
+        'init-e2e-flow',
+      }
+      vim.g['conjure#log#botright'] = true
+      vim.g['conjure#log#wrap'] = true
+      vim.g['conjure#client#clojure#nrepl#eval#auto_require'] = false
+      vim.g['conjure#client#clojure#nrepl#connection#auto_repl#enabled'] = false
+      vim.g['conjure#extract#tree_sitter#enabled'] = true
+      -- vim.g["conjure#client#clojure#nrepl#test#runner"] = "kaocha"
+      -- Make LSP diagnostics ignore log file
+      vim.cmd 'autocmd BufReadPost,BufNewFile conjure-log-* lua vim.diagnostic.disable(0)'
+    end,
+  },
+}
